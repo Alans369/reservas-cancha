6c33af264de17d09f403ef995723a7b2ffd5c600
Alan Edenilson Campos Sanchez <cs25001@esfe.agape.edu.sv>
2025-09-12 17:55:13 -0600

    GDC-21 creacion del servico de rol

--- src/rol/rol.service.ts
+++ src/rol/rol.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { CreateRolDto } from './dto/create-rol.dto';
-import { UpdateRolDto } from './dto/update-rol.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Rol } from './entities/rol.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RolService {
-  create(createRolDto: CreateRolDto) {
-    return 'This action adds a new rol';
+  constructor(
+    @InjectRepository(Rol)
+    private rolRepository: Repository<Rol>,
+  ) {}
+
+  async findAll(): Promise<Rol[]> {
+    return await this.rolRepository.find();
   }
 
-  findAll() {
-    return `This action returns all rol`;
-  }
+  async findById(id: number): Promise<Rol> {
+    const rl = await this.rolRepository.findOneBy({ id });
 
-  findOne(id: number) {
-    return `This action returns a #${id} rol`;
-  }
+    if (!rl) {
+      throw new Error('category not found');
+    }
 
-  update(id: number, updateRolDto: UpdateRolDto) {
-    return `This action updates a #${id} rol`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} rol`;
+    return rl;
   }
 }
