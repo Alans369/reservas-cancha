da0e0f998dce9c193d3c81e5b70e0e47f1072bc4
gerardo12465 <bs25001@esfe.agape.edu.sv>
2025-09-12 17:23:21 +0000

    GDC-18 se iso la creacion de edpoint pero no entiendo porque no aparecio en jira por eso hago este comit de emergencia

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
+
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -10,15 +11,19 @@ export class AuthController {
   @Post('/login')
   login(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.create(createAuthDto);
+
   }
 
   @Post('/register')
   register(@Body() createAuthDto: string) {
     return this.authService.findOne(+id);
+
   }
+
 
   @Post('/refresh')
   refreshToken(@Body() token: string) {
     return this.authService.remove(+id);
+    
   }
 }
