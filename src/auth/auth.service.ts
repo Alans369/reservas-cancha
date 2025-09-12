faab59867912ed1d330dc3e40d2d0e30fc018034
Alan Edenilson Campos Sanchez <cs25001@esfe.agape.edu.sv>
2025-09-12 17:54:18 -0600

    GDC-20 creacion del servico de auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,88 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UsuariosService } from 'src/usuarios/usuarios.service';
+
+import { LoginAuthDto } from './dto/login-auth.dto';
+import { Usuario } from 'src/usuarios/entities/usuario.entity';
+import { JwtService } from '@nestjs/jwt';
+import { AuthResponse } from './dto/utils-auth-dto';
+//import { AuthResponse } from './dto/utils-auth-dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private readonly jwtService: JwtService,
+    private userService: UsuariosService,
+  ) {}
+
+  async signIn(LoginAuthDto: LoginAuthDto) {
+    const { username, password } = LoginAuthDto;
+
+    const user = await this.userService.findOne(username);
+    if (!user) {
+      throw new Error('credenciales invalidas');
+    }
+
+    if (user?.password !== password) {
+      throw new Error('credneciales invalidas');
+    }
+
+    return user;
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async generateTokens(user: Usuario): Promise<AuthResponse> {
+    const payload = {
+      sub: user.id,
+      username: user.username,
+      role: user.rol.nombre,
+    };
+
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(payload),
+
+      this.jwtService.signAsync(payload, {
+        expiresIn: '2h', // refresh token dura 7 días
+        secret: process.env.JWT_REFRESH_SECRET, // usa un secreto diferente para más seguridad
+      }),
+    ]);
+
+    return {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  register(usario: Usuario): Promise<Usuario> {
+    return this.userService.create(usario);
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+  async refreshToken(token: string): Promise<{ access_token: string }> {
+    try {
+      // Verificar el refresh token
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_REFRESH_SECRET,
+      });
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+      // Eliminamos la fecha de expiración del payload
+      delete payload.exp;
+      delete payload.iat;
+
+      // Generar nuevo access token
+      const newAccessToken = await this.jwtService.signAsync(payload, {
+        expiresIn: '60s',
+      });
+
+      return {
+        access_token: newAccessToken,
+      };
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+        // Aquí puedes usar error.message, error.stack, etc.
+      }
+
+      throw new Error('Invalid refresh token');
+    }
   }
 }
