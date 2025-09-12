75f2c33d799e865f8b26c115f595c412ca42cc41
gerardo12465 <bs25001@esfe.agape.edu.sv>
2025-09-12 17:08:08 +0000

    GDC-17 crear Dto para las entidades de creacion de cuenta

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,32 @@
-export class CreateAuthDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateAuthDto {
+  @IsString()
+  @IsNotEmpty()
+  nombre: string;
+
+  @IsString()
+  apellido: string;
+
+  @IsString()
+  username: string;
+  
+  @IsString()
+  @IsNotEmpty()
+  telefono: string;
+  
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  rol: number;
+
+  
+}
