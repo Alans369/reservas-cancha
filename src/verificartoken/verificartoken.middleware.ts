fa7530057d767ada312cbe1622be8e0e268dc597
gerardo12465 <bs25001@esfe.agape.edu.sv>
2025-09-16 16:44:45 +0000

    GDC-28 creacion de middwalre para la autorizacon

---
+++ src/verificartoken/verificartoken.middleware.ts
@@ -0,0 +1,43 @@
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class VerificartokenMiddleware implements NestMiddleware {
+  constructor(private jwtService: JwtService) {}
+
+  async use(req: Request, res: Response, next: () => void) {
+    const token = this.extractTokenFromHeader(req);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+      });
+      // 💡 We're assigning the payload to the request object here
+      // so that we can access it in our route handlers
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+      req['UserId'] = payload.sub;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+      req['role'] = payload.role;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      console.log(payload.sub);
+    } catch {
+      throw new UnauthorizedException();
+    }
+
+    next();
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
