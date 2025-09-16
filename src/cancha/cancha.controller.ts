6656f9e011d7afec486d6adb0ecb64ce3b139733
Alan Campos <cs25001@esfe.agape.edu.sv>
2025-09-16 16:37:58 +0000

    GDC-26 creaciond e controlador de chmcha

---
+++ src/cancha/cancha.controller.ts
@@ -0,0 +1,129 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Req,
+  HttpException,
+  HttpStatus,
+  UsePipes,
+  ValidationPipe,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CanchaService } from './cancha.service';
+import { CreateCanchaDto } from './dto/create-cancha.dto';
+import { UpdateCanchaDto } from './dto/update-cancha.dto';
+import { UsuariosService } from 'src/usuarios/usuarios.service';
+import { Usuario } from 'src/usuarios/entities/usuario.entity';
+import { ApiBearerAuth } from '@nestjs/swagger';
+@ApiBearerAuth()
+@Controller('api/v1/cancha')
+export class CanchaController {
+  constructor(
+    private readonly canchaService: CanchaService,
+    private readonly usuarioService: UsuariosService,
+  ) { }
+
+  @Post()
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  async create(
+    @Body() createCanchaDto: CreateCanchaDto,
+    @Req() req: Request & { UserId?: string },
+  ) {
+    try {
+      const user = await this.usuarioService.findById(
+        parseInt(req.UserId as string),
+      );
+
+      const cancha: CreateCanchaDto = {
+        ...createCanchaDto,
+        propetario: user as Usuario,
+      };
+
+      return this.canchaService.create(cancha);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      throw new HttpException(
+        'error al  crear la cancha',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  @Get()
+  async findAll(@Query('page') page: string, @Query('pageSize') pageSize: string, @Req() req: Request & { UserId?: string }) {
+  try {
+    const user = await this.usuarioService.findById(
+      parseInt(req.UserId as string),
+    );
+
+    return this.canchaService.findAll(user as Usuario,+page,+pageSize);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    throw new HttpException(
+      'error al  crear la cancha',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+
+@Get('/cliente')
+async findAllcliente() {
+  try {
+    return this.canchaService.findAllcliente();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    throw new HttpException(
+      'error al  crear la cancha',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+
+@Get(':id')
+async findOne(@Param('id') id: string) {
+  try {
+    return await this.canchaService.findOne(+id);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    throw new HttpException(
+      'error al  crear la cancha',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+
+@Put(':id')
+@UsePipes(new ValidationPipe({ whitelist: true }))
+async update(
+  @Param('id') id: string,
+  @Body() updateCanchaDto: UpdateCanchaDto,
+) {
+  try {
+    return await this.canchaService.update(+id, updateCanchaDto);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    throw new HttpException(
+      'error al  crear la cancha',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+
+@Delete(':id')
+async remove(@Param('id') id: string) {
+  try {
+    return await this.canchaService.remove(+id);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    throw new HttpException(
+      'error al  crear la cancha',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+}
